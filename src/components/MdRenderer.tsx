1ab2a6ec2c1967d2442fec867f4641002f2bb3af
elomarii <i.ibrahim.elomari@gmail.com>
2024-12-19 22:30:10 +0100

    minor tweaks to md renderer

--- src/components/MdRenderer.tsx
+++ src/components/MdRenderer.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Markdown from "react-markdown";
-
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/cjs/styles/prism";
 interface Props {
   path: string;
 }
@@ -12,7 +14,25 @@ function MdRenderer({ path }: Props) {
     .then((text) => setMarkdown(text));
   return (
     <article>
-      <Markdown>{markdown}</Markdown>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code: ({ className, children, ...props }) => {
+            const match = /language-(\w+)/.exec(className || "");
+            return match ? (
+              <SyntaxHighlighter language={match[1]} style={tomorrow}>
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {markdown}
+      </Markdown>
     </article>
   );
 }
