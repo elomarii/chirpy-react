bdd83679e8f85d05256efca277d36a929478ba14
elomarii <i.ibrahim.elomari@gmail.com>
2024-12-24 18:24:57 +0100

    routes for categories

--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,15 @@ import Whoami from "./pages/Whoami.tsx";
 import Post from "./pages/Post.tsx";
 import Blog from "./pages/Blog.tsx";
 import Projects from "./pages/Projects.tsx";
+import { readMarkdown } from "./utils.tsx";
 
 // Define all routes and respective components
 const articles = import.meta.glob([
   "/public/posts/*.md",
   "/public/projects/*.md",
 ]);
+
+// Routes for articles
 const articlesRoutes = Object.keys(articles).map((filepath, _index) => {
   const path = filepath.replace("/public", "");
   return {
@@ -22,6 +25,49 @@ const articlesRoutes = Object.keys(articles).map((filepath, _index) => {
     element: <Post path={path} />,
   };
 });
+
+// Routes for categories
+const fetchCategories = async () => {
+  const fmatters = await Promise.all(
+    Object.keys(articles).map((path) =>
+      fetch(path.replace("/public", ""))
+        .then((response) => response.text())
+        .then((text) => {
+          const fm = readMarkdown(text).frontMatter;
+          return {
+            title: fm.title,
+            categories: fm.categories,
+            date: fm.date,
+            path: path,
+          };
+        })
+    )
+  );
+  let catList: string[] = [];
+  const categories = new Map();
+  fmatters.map((fm) => {
+    catList = catList.concat(fm.categories);
+    categories.set(
+      fm.categories[0],
+      (categories.get(fm.categories[0]) ?? new Set()).union(
+        new Set(fm.categories.slice(1))
+      )
+    );
+  });
+  catList = [...new Set(catList)];
+  return { categories, catList, fmatters };
+};
+
+export const { categories, catList, fmatters } = await fetchCategories();
+
+const categoriesRoutes = catList.map((cat, _index) => {
+  return {
+    path: `/categories/${cat}`,
+    element: <h1>{cat}</h1>,
+  };
+});
+
+// Router
 const router = createBrowserRouter([
   {
     path: "/",
@@ -41,6 +87,7 @@ const router = createBrowserRouter([
     element: <Whoami />,
   },
   ...articlesRoutes,
+  ...categoriesRoutes,
 ]);
 
 createRoot(document.getElementById("root")!).render(
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,5 +1,23 @@
+import { categories } from "../main";
+
 function Blog() {
-  return "Blog";
+  // get all root categories
+  return (
+    <ul>
+      {Array.from(categories?.keys() ?? []).map((key, index) => (
+        <li key={`cat-${index}`}>
+          <a href={`categories/${key}`}>{key}</a>
+          <ul>
+            {[...(categories?.get(key) ?? [])].map((cat) => (
+              <li key={"subcat-" + cat}>
+                <a href={`categories/${cat}`}>{cat}</a>
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Blog;
