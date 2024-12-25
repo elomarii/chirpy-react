b57feddb019d6945d696259332b89d700431eece
elomarii <i.ibrahim.elomari@gmail.com>
2024-12-25 22:52:41 +0100

    globals

--- src/globals.tsx
+++ src/globals.tsx
@@ -3,3 +3,16 @@ export const site = {
   description: "Jnior Iformatik",
   avatar: "/assets/avatar.png",
 };
+
+export const socials = {
+  name: "Ibrahim El Omari",
+  github: "elomarii",
+  linkedin: "ibrahim-el-omari",
+  email: "",
+};
+
+export const copyright = {
+  brief: "All rights reserved.",
+  verbose:
+    "Except where otherwise noted, the blog posts on this site are licensed under the Creative Commons Attribution 4.0 International (CC BY 4.0) License by the author.",
+};
