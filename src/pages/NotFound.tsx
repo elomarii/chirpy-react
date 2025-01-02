dd8631a30d1ce0ddc4b9ce57ced2e63b8198e30b
elomarii <i.ibrahim.elomari@gmail.com>
2025-01-02 12:12:15 +0100

    not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,34 @@
+const banner: string = `
+███╗   ██╗ ██████╗ ████████╗    ███████╗ ██████╗ ██╗   ██╗███╗   ██╗██████╗ 
+████╗  ██║██╔═══██╗╚══██╔══╝    ██╔════╝██╔═══██╗██║   ██║████╗  ██║██╔══██╗
+██╔██╗ ██║██║   ██║   ██║       █████╗  ██║   ██║██║   ██║██╔██╗ ██║██║  ██║
+██║╚██╗██║██║   ██║   ██║       ██╔══╝  ██║   ██║██║   ██║██║╚██╗██║██║  ██║
+██║ ╚████║╚██████╔╝   ██║       ██║     ╚██████╔╝╚██████╔╝██║ ╚████║██████╔╝
+╚═╝  ╚═══╝ ╚═════╝    ╚═╝       ╚═╝      ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═════╝
+`;
+
+const four0four: string = `
+██╗  ██╗ ██████╗ ██╗  ██╗
+██║  ██║██╔═████╗██║  ██║
+███████║██║██╔██║███████║
+╚════██║████╔╝██║╚════██║
+     ██║╚██████╔╝     ██║
+     ╚═╝ ╚═════╝      ╚═╝
+`;
+
+const style: React.CSSProperties = {
+  font: "0.8em Inconsolata, monospace",
+  lineHeight: "1.15em",
+  marginTop: "2em",
+};
+
 export default function NotFound() {
-  return <h2>Reached the rabbit hole.. Go back now!</h2>;
+  return (
+    <>
+      <pre style={style}>{four0four}</pre>
+      <pre style={{ ...style, marginTop: "-2em" }}>{banner}</pre>
+      <h2>Oops, you've hit a dead end!</h2>
+      <p>It looks like this page packed its bags and left.</p>
+    </>
+  );
 }
