4c11089b80e838e9d848b27b946b560da37aaf1d
elomarii <i.ibrahim.elomari@gmail.com>
2025-01-05 15:58:44 +0100

    testing build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext"
+  },
   plugins: [react(),
     {
       name: 'markdown-loader',
