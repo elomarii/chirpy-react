ed0b7327a1122728d7567e1a47f998c3299ab6b2
elomarii <i.ibrahim.elomari@gmail.com>
2025-01-08 19:30:40 +0100

    toc observer

--- src/components/Toc.tsx
+++ src/components/Toc.tsx
@@ -1,21 +1,39 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../state/store";
 import { headerToId } from "../utils";
+import { useState } from "react";
 
 export default function Toc() {
   const tocHeaders = useSelector((state: RootState) => state.toc.headers);
+  const [activeId, setActiveId] = useState("");
 
-  return (
-    <section
-      id="toc-wrapper"
-      className={tocHeaders.length > 0 ? "ps-0 pe-4" : "d-none ps-0 pe-4"}
-    >
+  // have issues selecting headers right after rendering of the page
+  setTimeout(() => {
+    const observer = new IntersectionObserver((entries) =>
+      entries.forEach((e) => {
+        if (e.isIntersecting) {
+          setActiveId(e.target.id);
+        }
+      })
+    );
+    document
+      .querySelectorAll(".content h2")
+      .forEach((header) => observer.observe(header));
+  }, 800);
+
+  return tocHeaders.length > 0 ? (
+    <section id="toc-wrapper" className="ps-0 pe-4">
       <h2 className="panel-heading ps-3 mb-2">Contents</h2>
       <nav id="toc">
         <ul className="toc-list">
           {tocHeaders.map((h, idx) => (
             <li className="toc-list-item" key={idx}>
-              <a href={`#${headerToId(h)}`} className="toc-link node-name--H2">
+              <a
+                href={`#${headerToId(h)}`}
+                className={`toc-link node-name--H2 ${
+                  activeId == headerToId(h) ? "is-active-link" : ""
+                }`}
+              >
                 {h}
               </a>
             </li>
@@ -23,5 +41,7 @@ export default function Toc() {
         </ul>
       </nav>
     </section>
+  ) : (
+    <></>
   );
 }
